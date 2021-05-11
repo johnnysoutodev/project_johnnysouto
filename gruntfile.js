029bca7b1d09bf242da3a68103a89271069de869
Johnny Souto <39660140+johnnysoutodev@users.noreply.github.com>
2021-05-10 23:47:32 -0300

    Update gruntfile.js
    mudando nome de iniciando por compilar

--- gruntfile.js
+++ gruntfile.js
@@ -152,9 +152,9 @@ module.exports = function(grunt){
     grunt.registerTask('compactando-images', ['clean:images', 'image:dynamic', 'copy:images']);
 
     // Tarefas para trabalhar no projeto
-    grunt.registerTask('iniciando', ['limpar-tudo','compactando-images','estilizando', 'codificando', 'copy:files']);
+    grunt.registerTask('compilar', ['limpar-tudo','compactando-images','estilizando', 'codificando', 'copy:files']);
     grunt.registerTask('estilizando', ['concat:css','cssmin', 'copy:css']);
     grunt.registerTask('codificando', ['concat:js','uglify', 'copy:js']);
-    grunt.registerTask('publicando-projeto', ['limpar-tudo','compactando-images','codificando','estilizando','copy:files']);
+    grunt.registerTask('publicando-projeto', ['limpar-tudo','compactando-images','estilizando','codificando','copy:files']);
     grunt.registerTask('default', ['watch']);
 };
