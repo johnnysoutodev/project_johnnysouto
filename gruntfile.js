931e3b0402bd743b2cfd5d6ed02052999286b0fc
Johnny Souto <39660140+johnnysoutodev@users.noreply.github.com>
2021-05-11 00:44:11 -0300

    Update gruntfile.js
    renomeando a task publicando-projeto para publish

--- gruntfile.js
+++ gruntfile.js
@@ -155,6 +155,6 @@ module.exports = function(grunt){
     grunt.registerTask('compilar', ['limpar-tudo','compactando-images','estilizando', 'codificando', 'copy:files']);
     grunt.registerTask('estilizando', ['concat:css','cssmin', 'copy:css']);
     grunt.registerTask('codificando', ['concat:js','uglify', 'copy:js']);
-    grunt.registerTask('publicando-projeto', ['limpar-tudo','compactando-images','estilizando','codificando','copy:files']);
+    grunt.registerTask('publish', ['limpar-tudo','compactando-images','estilizando','codificando','copy:files']);
     grunt.registerTask('default', ['watch']);
 };
