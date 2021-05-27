378f7dee1565276c75f929cc6796065dfaa4e962
Johnny Souto <39660140+johnnysoutodev@users.noreply.github.com>
2021-05-27 12:47:48 -0300

    Update gruntfile.js
    ordem dos arquivos de .js priorizando o fontawesome

--- gruntfile.js
+++ gruntfile.js
@@ -58,7 +58,7 @@ module.exports = function(grunt){
         concat: {
 
             js: {
-                src: ['src/js/analytics', 'src/js/**/*.js'],
+                src: ['src/js/analytics', 'src/js/fontawesome.js', 'src/js/**/*.js'],
                 dest: '.tmp/js/scripts.min.js'
             },
 
