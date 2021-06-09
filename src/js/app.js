5f85f652f5cb49d56a6582166f9f704e75cf9bc9
Johnny Souto <39660140+johnnysoutodev@users.noreply.github.com>
2021-06-09 13:19:51 -0300

    Update app.js
    add API Amazon for request IP

--- src/js/app.js
+++ src/js/app.js
@@ -42,4 +42,10 @@ function verifiyLang(){
     }
 }
 
+function getIPFromAmazon() {
+    fetch("https://checkip.amazonaws.com/").then(res => res.text()).then(data => console.log(data))
+}
+
+getIPFromAmazon();
+
 verifiyLang();
