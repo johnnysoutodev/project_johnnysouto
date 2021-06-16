955d6dfebeab2d932951ca9b0dc76b76867ef821
Johnny Souto <39660140+johnnysoutodev@users.noreply.github.com>
2021-06-16 01:48:19 -0300

    Update app.js

--- src/js/app.js
+++ src/js/app.js
@@ -4,14 +4,12 @@ var year = new Date().getFullYear();
 
 var age = 0;
 
-const language = navigator.language;
-const appVersion = navigator.appVersion;
-const browserEngine = navigator.product;
-const browserUserAgent = navigator.userAgent;
-
 function calcAge(year) {
     return age = year -1982;
 }
+
+calcAge(year);
+
 
 const colorSwitch = document.getElementById('input-color-switch');
 
@@ -33,41 +31,3 @@ function darkModeOn(){
 function darkModeOff(){
     document.body.classList.remove("dark-mode");
 }
-
-calcAge(year);
-
-function verifiyLang(){
-    if(language === 'pt-br'){
-        // location.replace("https://www.johnnysouto.com.br/");
-    }
-}
-
-verifiyLang();
-
-// Verificar como funcionar o AWS IP
-
-/*
-function getIPFromAmazon() {
-    fetch("https://checkip.amazonaws.com/").then(res => res.text()).then(data => console.log(data))
-}
-
-getIPFromAmazon();
-
-*/
-
-// Para verificar como usar orequire
-/*
-const http = require('http');
-
-function requestListener(req, res) {
-  let forwarded = req.headers['x-forwarded-for']
-  let ip = forwarded ? forwarded.split(/, /)[0] : req.connection.remoteAddress;
-  res.writeHead(200);
-  res.end(ip);
-}
-
-const server = http.createServer(requestListener);
-server.listen(3000);
-*/
-
-// console.log('Server listening at http://localhost:3000');
