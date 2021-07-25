057d9e273b4144d440e6bdac64ccb27f4b85c842
Johnny Souto <39660140+johnnysoutodev@users.noreply.github.com>
2021-07-25 12:12:09 -0300

    Update init.js

--- public/js/init.js
+++ public/js/init.js
@@ -4,10 +4,8 @@ const language = navigator.language;
 
 function verifiyLang(){
     if(language == 'pt-br'){
-        // location.replace("https://www.johnnysouto.com.br/br/");
         location.replace("/pt/");
     } else {
-        // location.replace("https://www.johnnysouto.com.br/en/");
         location.replace("/en/");
     }
 }
