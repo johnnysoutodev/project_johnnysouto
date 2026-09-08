ff53c2daafbe2f0bc3246cac6ab18a53b692cba0
Johnny Souto <39660140+johnnysoutodev@users.noreply.github.com>
2026-09-08 04:08:49 -0300

    feat(scroll-to-top): adiciona botão flutuante de voltar ao topo

--- angular-app/src/app/app.ts
+++ angular-app/src/app/app.ts
@@ -2,9 +2,10 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from './layout/header/header';
 import { Footer } from './layout/footer/footer';
+import { ScrollToTop } from './layout/scroll-to-top/scroll-to-top';
 
 @Component({
-  imports: [RouterOutlet, Header, Footer],
+  imports: [RouterOutlet, Header, Footer, ScrollToTop],
   selector: 'app-root',
   styleUrl: './app.scss',
   templateUrl: './app.html',
